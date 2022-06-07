cf2f41c91e43716ca27865059a7401ff93157eb2
Jorge M'Moller <jlmm.jlmm76@gmail.com>
2022-06-07 13:25:37 -0500

    se creo la pagina de incio

---
+++ coder/coder/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+from coder.settings import BASE_DIR
+
+def home(request):
+    #return HttpResponse(BASE_DIR/"TEMPLATES")
+    return render(request, 'TEMPLATES\index.html')
