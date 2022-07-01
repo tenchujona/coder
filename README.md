da826d1c6f0bd982e33857a40b173ea222e931cd
Jorge M'Moller <jlmm.jlmm76@gmail.com>
2022-07-01 17:59:55 -0500

    se corrigio parte del codigo de edicion de perfil
    ya no se puede acceder a la pagina crear empresa sin estar autenticado

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # coder
 La pagina funciona como un blog donde podes registrarte y tambien buscar usuarios o empresas a travez de la barra de busqueda. Cuenta con un login para ingresar y tambien una pagina de inicio.
 
-- Para crear un perfil de empresa debe registarse y estar logeado en la pagina(sin embargo se puede entrar directamente desde la url)<br />
+- Para crear un perfil de empresa debe registarse y estar logeado en la pagina<br />
 - Se puede ver las categorias y separarlos<br />
 - Se peude buscar por nombres, email o nombre de usuario<br />
 - En la página de registro de empresa y usuario, si se intenta registrar una empresa o usario con un nombre que ya existente, la página mostrará un mensaje de advertencia. Si se ocurre un error mostrará un mensaje dicendo que ha ocurrido un error.<br />
 - Edición de perfil de usario, se puede añadir más datos del usario <br />
+- Si no estas logeado no puedes entrar en la página de perfil <br />
+- Si cambias la contraseña se cerrará tu sesión y serás redireccionado a la página de login <br />
 
 LAS PAGINAS SON:
+
+Usuario: Monica
+Contraseña: testuser
 
 http://127.0.0.1:8000/<br />
 http://127.0.0.1:8000/login/<br />
--- coder/app1/views.py
+++ coder/app1/views.py
@@ -116,104 +116,108 @@ def categori_business(request):
     return render(request, "all_business.html", context=context)
 
 def create_business(request):
-    if request.method == "POST":
-        form = Empresa_form(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            try:
-                print(request.FILES)
-                filepath = request.FILES.get('image') # VERIFICA SI SE ENVIARON ARCHIVOS - SI MultiValueDict CONTIENE EL IMAGE ES VERDAD
-                if filepath:
-                    new_content = Empresas.objects.create(
-                        name = form.cleaned_data["name"],
-                        email = form.cleaned_data["email"],
-                        CEO = form.cleaned_data["CEO"],
-                        ubicacion = form.cleaned_data["ubicacion"],
-                        numero = form.cleaned_data["numero"],
-                        category_id = 2,
-                        image = form.cleaned_data["image"],
-                    )
-                else:
-                    new_content = Empresas.objects.create(
-                        name = form.cleaned_data["name"],
-                        email = form.cleaned_data["email"],
-                        CEO = form.cleaned_data["CEO"],
-                        ubicacion = form.cleaned_data["ubicacion"],
-                        numero = form.cleaned_data["numero"],
-                        category_id = 2,
-                    )
-                business = Empresas.objects.all()
-                context = {"new_content":new_content, "business":business}
-                return render(request, "all_business.html", context=context)
-            except IntegrityError:
-                exists = True
-                context = {"exists":exists}
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = Empresa_form(data=request.POST, files=request.FILES)
+            if form.is_valid():
+                try:
+                    print(request.FILES)
+                    filepath = request.FILES.get('image') # VERIFICA SI SE ENVIARON ARCHIVOS - SI MultiValueDict CONTIENE EL IMAGE ES VERDAD
+                    if filepath:
+                        new_content = Empresas.objects.create(
+                            name = form.cleaned_data["name"],
+                            email = form.cleaned_data["email"],
+                            CEO = form.cleaned_data["CEO"],
+                            ubicacion = form.cleaned_data["ubicacion"],
+                            numero = form.cleaned_data["numero"],
+                            category_id = 2,
+                            image = form.cleaned_data["image"],
+                        )
+                    else:
+                        new_content = Empresas.objects.create(
+                            name = form.cleaned_data["name"],
+                            email = form.cleaned_data["email"],
+                            CEO = form.cleaned_data["CEO"],
+                            ubicacion = form.cleaned_data["ubicacion"],
+                            numero = form.cleaned_data["numero"],
+                            category_id = 2,
+                        )
+                    business = Empresas.objects.all()
+                    context = {"new_content":new_content, "business":business}
+                    return render(request, "all_business.html", context=context)
+                except IntegrityError:
+                    exists = True
+                    context = {"exists":exists}
+                    return render(request, "create_business.html", context=context)
+            else:
+                print(form.errors)
+                error = True
+                context = {"error":error}
                 return render(request, "create_business.html", context=context)
-        else:
-            print(form.errors)
-            error = True
-            context = {"error":error}
-            return render(request, "create_business.html", context=context)
 
-    form = Empresa_form()
-    context = {"form":form}
-    return render(request, "create_business.html", context=context)
+        form = Empresa_form()
+        context = {"form":form}
+        return render(request, "create_business.html", context=context)
+    else:
+        return redirect('home')
 
 def profile(request):
-
-    if request.method == "POST":
-        form = Edit_form(request.POST)
-        if form.is_valid() and request.POST["password1"]==request.POST["password2"]:
-            try:
-                UsermodelUpdate = User.objects.get(pk=request.user.id)
-                Data_UsermodelUpdate = Data_Users.objects.get(user_id=request.user.id)
-                Data_UsermodelUpdate.gender = request.POST["genero"]
-                filepath = request.FILES.get('image')
-                
-                if request.POST["username"] != "":
-                    UsermodelUpdate.username = request.POST["username"]
-                if request.POST["password1"] != "":
-                    UsermodelUpdate.set_password(form.cleaned_data['password2'])
-                    password_reset = True
-                if request.POST["email"] != "":
-                    UsermodelUpdate.email = form.cleaned_data["email"]
-                if request.POST["first_name"] != "":
-                    UsermodelUpdate.first_name = request.POST["first_name"]
-                if request.POST["last_name"] != "":
-                    UsermodelUpdate.last_name = request.POST["last_name"]
-                if request.POST["username"] != "":
-                    Data_UsermodelUpdate.address = form.cleaned_data["address"]
-                if request.POST["ocupacion"] != "":
-                    Data_UsermodelUpdate.ocupacion = form.cleaned_data["ocupacion"]
-                if request.POST["numero"] != "":
-                    Data_UsermodelUpdate.numero = form.cleaned_data["numero"]
-                if request.POST["pais"] != "":
-                    Data_UsermodelUpdate.pais = form.cleaned_data["pais"]
-                if request.POST["ciudad"] != "":
-                    Data_UsermodelUpdate.ciudad = form.cleaned_data["ciudad"]
-                if filepath:
-                    Data_UsermodelUpdate.image = request.FILES.get("image")
-                
-                UsermodelUpdate.save()
-                Data_UsermodelUpdate.save()
-                success = True
-                context = {"success": success}
-                if password_reset:
-                    count = range(10)
-                    context = {"success": success, "password_reset":password_reset, "count":count}
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = Edit_form(request.POST)
+            if form.is_valid() and request.POST["password1"]==request.POST["password2"]:
+                try:
+                    UsermodelUpdate = User.objects.get(pk=request.user.id)
+                    Data_UsermodelUpdate = Data_Users.objects.get(user_id=request.user.id)
+                    Data_UsermodelUpdate.gender = request.POST["genero"]
+                    filepath = request.FILES.get('image')
+                    
+                    if request.POST["username"] != "":
+                        UsermodelUpdate.username = request.POST["username"]
+                    if request.POST["password1"] != "":
+                        UsermodelUpdate.set_password(form.cleaned_data['password2'])
+                        password_reset = True
+                    if request.POST["email"] != "":
+                        UsermodelUpdate.email = form.cleaned_data["email"]
+                    if request.POST["first_name"] != "":
+                        UsermodelUpdate.first_name = request.POST["first_name"]
+                    if request.POST["last_name"] != "":
+                        UsermodelUpdate.last_name = request.POST["last_name"]
+                    if request.POST["username"] != "":
+                        Data_UsermodelUpdate.address = form.cleaned_data["address"]
+                    if request.POST["ocupacion"] != "":
+                        Data_UsermodelUpdate.ocupacion = form.cleaned_data["ocupacion"]
+                    if request.POST["numero"] != "":
+                        Data_UsermodelUpdate.numero = form.cleaned_data["numero"]
+                    if request.POST["pais"] != "":
+                        Data_UsermodelUpdate.pais = form.cleaned_data["pais"]
+                    if request.POST["ciudad"] != "":
+                        Data_UsermodelUpdate.ciudad = form.cleaned_data["ciudad"]
+                    if filepath:
+                        Data_UsermodelUpdate.image = request.FILES.get("image")
+                    
+                    UsermodelUpdate.save()
+                    Data_UsermodelUpdate.save()
+                    success = True
+                    context = {"success": success}
+                    if password_reset:
+                        count = range(10)
+                        context = {"success": success, "password_reset":password_reset, "count":count}
+                    return render(request, "profile.html", context=context)
+                except IntegrityError:
+                    exist = True
+                    context = {"exist": exist}
+                    return render(request, "profile.html", context=context)
+            elif form.cleaned_data["password1"] != form.cleaned_data["password2"]:
+                not_match = True
+                context = {"not_match": not_match}
                 return render(request, "profile.html", context=context)
-            except IntegrityError:
-                exist = True
-                context = {"exist": exist}
-                return render(request, "profile.html", context=context)
-
-        elif form.cleaned_data["password1"] != form.cleaned_data["password2"]:
-            not_match = True
-            context = {"not_match": not_match}
-            return render(request, "profile.html", context=context)
+            else:
+                error = True
+                context = {"error": error}
+                return render(request, "profile.html")
         else:
             return render(request, "profile.html")
-    if request.user.is_authenticated:
-        return render(request, "profile.html")
     else:
         return redirect('home')
 
