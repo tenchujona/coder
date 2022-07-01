5f576885ad52381a2ab01b8471864f8eeaeeded3
Jorge M'Moller <jlmm.jlmm76@gmail.com>
2022-07-01 18:13:27 -0500

    se ordeno las urls en sus respectivas apps

--- coder/coder/urls.py
+++ coder/coder/urls.py
@@ -13,26 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import views
-from django.contrib import admin
-from django.urls import path
-from coder.views import home
-from app1.views import *
-
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+
+from coder.views import home
+from app1.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('login', login_view, name="login"),
-    path('buscar/', search_view, name="login"),
-    path('all-users/', categori_users, name="login"),
-    path('all-business/', categori_business, name="login"),
-    path('create-business/', create_business, name="login"),
-    path('signup', create_user, name="login"),
-    path('logout', logout_view, name="logout"),
-    path('my-profile/', profile, name="mi perfil"),
+    path('', include("app1.urls"), name="extends url"),
 ]
 
 #IMAGES
